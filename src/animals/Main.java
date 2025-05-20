5aa0e574e8dcb885d669d35bd2928d35c4f8c11b
Bruno0190 <bruno.varadero90@gmail.com>
2025-05-20 14:42:57 +0200

    feat: animali

--- src/animals/Main.java
+++ src/animals/Main.java
@@ -9,17 +9,48 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Scegli un animale tra Cane, Delfino, Aquila o Passerotto");
-        String animale = input.nextLine();
-        if (animale.toLowerCase().equals("cane") || animale.toLowerCase().equals("delfino") || animale.toLowerCase().equals("aquila") || animale.toLowerCase().equals("passerotto")) {
+        String animaleScelto = input.nextLine().toLowerCase();
 
-            System.out.println("Come si chiama il tuo " + animale + "?");
+        if (animaleScelto.equals("cane") || animaleScelto.equals("delfino") ||
+            animaleScelto.equals("aquila") || animaleScelto.equals("passerotto")) {
+
+            System.out.println("Come si chiama il tuo " + animaleScelto + "?");
             String nome = input.nextLine();
-            
 
+            System.out.println("A che sottospecie appartiene?");
+            String specie = input.nextLine();
+            //Variabile d'appoggio, sempre variabile d'appoggio
+            Animale mioAnimale = null;
+
+            switch (animaleScelto) {
+                case "cane":
+                    mioAnimale = new Cane(nome, specie);
+                    break;
+                case "delfino":
+                    mioAnimale = new Delfino(nome, specie);
+                    break;
+                case "aquila":
+                    mioAnimale = new Aquila(nome, specie);
+                    break;
+                case "passerotto":
+                    mioAnimale = new Passerotto(nome, specie);
+                    break;
+            }
+
+            // Stampa solo se l'oggetto è stato creato
+            if (mioAnimale != null) {
+                System.out.println("Il tuo animale è un " + animaleScelto +
+                    ". Si chiama " + nome + ", di specie " + specie +
+                    ". Si nutre di " + mioAnimale.mangia() +
+                    ", il verso che fa è: " + mioAnimale.verso() +
+                    " e quando dorme fa: " + mioAnimale.dormi());
+            }
+
+        } else {
+            System.out.println("Animale non riconosciuto.");
         }
-
 
         input.close();
     }
-
 }
+
